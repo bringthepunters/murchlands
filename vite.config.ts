c20fb1788a24d1b6f5aa5f26abef9e1290693ee5
bringthepunters <155452570+bringthepunters@users.noreply.github.com>
2024-12-24 21:39:34 +1100

    added JSON data store

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/murchlands/', // Updated to match your repo name
+  base: '/murchlands/', // Must match your GitHub repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
