2188808ae0c262bbcb1ce01fb2ee9eafdef48da6
bringthepunters <155452570+bringthepunters@users.noreply.github.com>
2024-12-24 22:07:16 +1100

    deply

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/murchlands/', // Must match your GitHub repository name
+  base: '/murchlands/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
-
